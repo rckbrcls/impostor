7b2528e9541888583d76239bc6f33d3927d13230
brcls <petrick_barcelos@hotmail.com>
2026-01-09 13:10:12 -0300

    supa base config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 // NEXT_PUBLIC_SUPABASE_URL=sua_url_do_supabase
 // NEXT_PUBLIC_SUPABASE_ANON_KEY=sua_anon_key_do_supabase
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY || "";
 
 // Criar cliente apenas se as credenciais estiverem configuradas
 export const supabase =
