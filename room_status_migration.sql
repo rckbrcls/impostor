b3229a83f1ad1da3760f587dd126fa894b9dfb1a
brcls <petrick_barcelos@hotmail.com>
2026-01-11 16:02:22 -0300

    delete

--- room_status_migration.sql
+++
@@ -1,8 +0,0 @@
--- Add status column to rooms table
-ALTER TABLE rooms ADD COLUMN IF NOT EXISTS status TEXT DEFAULT 'waiting';
-
--- Update existing rooms to have a status
-UPDATE rooms SET status = 'waiting' WHERE status IS NULL;
-
--- Make it check for valid values (optional but good practice)
-ALTER TABLE rooms ADD CONSTRAINT check_room_status CHECK (status IN ('waiting', 'playing', 'game_finished'));
